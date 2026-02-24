4f8285b95eb71d311171a6859c3eea4f753b6dab
Ella Sehn <esehn@lcs.on.ca>
2026-02-24 14:22:46 -0500

    Changed text

---
+++ Clock/Views/WorldClockView.swift
@@ -0,0 +1,18 @@
+//
+//  WorldClockView.swift
+//  Clock
+//
+//  Created by Ella Sehn on 2026-02-24.
+//
+
+import SwiftUI
+
+struct WorldClockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorldClockView()
+}
